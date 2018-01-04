ff3f60dbda14acb3c1cc5a921c84f3794a369b6f
Quang Nguyen <quang@gmail.com>
2018-01-05 02:43:25 +0700

    change locate of index.html

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static(__dirname + '/public'));
 // set the home page route
 app.get('/', function(req, res) {
 
-  res.sendFile(path.join(__dirname + '/public/index.html'));
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(port, function() {
